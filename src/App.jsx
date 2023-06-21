9aafe671675c3c80cbc36435d1f64168e0bc072b
MerlinConnected <gaetanjestin@outlook.com>
2023-06-21 11:09:27 +0200

    feat: scroll animation w/ TheatreJS

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,19 @@
-import { OrbitControls, useGLTF, useTexture, Environment, Sky, BakeShadows, useHelper, Stage } from '@react-three/drei'
+import { Sky, ScrollControls, Gltf, useScroll } from '@react-three/drei'
 import { Canvas, useFrame } from '@react-three/fiber'
-import React, { useRef, useState } from 'react'
-import { EffectComposer, N8AO, TiltShift2 } from '@react-three/postprocessing'
+import { EffectComposer, N8AO } from '@react-three/postprocessing'
+import { getProject, val } from '@theatre/core'
+import { SheetProvider, PerspectiveCamera, useCurrentSheet } from '@theatre/r3f'
 
-const Model = () => {
-	const ref = useRef()
-	const { nodes } = useGLTF('/landscape.glb')
-	const texture = useTexture('/tex/sprite.png')
-
-	const [camActive, set] = useState(true)
-
-	useFrame((state) => {
-		state.camera.position.lerp({ x: camActive ? 0.25 : -0.35, y: camActive ? 0.1 : 0, z: camActive ? -0.15 : 0.45 }, 0.03)
-		state.camera.lookAt(camActive ? 0.5 : -0.25, camActive ? -0.15 : 0.13, camActive ? 0.12 : -0.45)
-		// state.camera.lookAt(0.5, 0, 0.07)
-	})
+export default function App() {
+	const sheet = getProject('Fly Through').sheet('Scene')
 
 	return (
-		<>
-			<group dispose={null}>
-				<points geometry={nodes.landscape.geometry}>
-					<pointsMaterial color='#9fcdd8' size={0.002} sizeAttenuation={true} map={texture} transparent={true} />
-				</points>
-				<mesh castShadow receiveShadow ref={ref} geometry={nodes.landscape.geometry} position={[0, -0.0005, 0]} onClick={() => set(!camActive)}>
-					<meshPhysicalMaterial color='white' wireframe={false} flatShading={true} castShadow receiveShadow />
-				</mesh>
-			</group>
-		</>
-	)
-}
-
-const Scene = () => {
-	return (
-		<Canvas shadows camera={{ position: [20, 20, 20], fov: 30, near: 0.01 }}>
-			<Stage
-				intensity={0.5}
-				preset='rembrandt'
-				shadows={{ type: 'accumulative', color: 'skyblue', colorBlend: 2, opacity: 1 }}
-				environment='city'
-				center={false}
-			>
-				<Model castShadow receiveShadow />
-			</Stage>
-			{/* <Tracker /> */}
-			{/* <OrbitControls enableZoom={true} /> */}
+		<Canvas gl={{ preserveDrawingBuffer: true }}>
+			<ScrollControls pages={5}>
+				<SheetProvider sheet={sheet}>
+					<Scene />
+				</SheetProvider>
+			</ScrollControls>
 			<EffectComposer disableNormalPass multisampling={0}>
 				<N8AO aoRadius={0.1} intensity={2} aoSamples={6} denoiseSamples={4} />
 			</EffectComposer>
@@ -52,4 +22,21 @@ const Scene = () => {
 	)
 }
 
-export default Scene
+function Scene() {
+	const sheet = useCurrentSheet()
+	const scroll = useScroll()
+
+	useFrame(() => {
+		const sequenceLength = val(sheet.sequence.pointer.length)
+		sheet.sequence.position = scroll.offset * sequenceLength
+	})
+
+	return (
+		<>
+			<ambientLight intensity={0.5} />
+			<directionalLight position={[-5, 5, -5]} intensity={1.5} />
+			<Gltf src='landscape.glb' castShadow receiveShadow />
+			<PerspectiveCamera theatreKey='Camera' makeDefault position={[0, 0, 0]} fov={90} near={0.1} far={70} />
+		</>
+	)
+}
